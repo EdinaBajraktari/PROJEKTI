ffc5cfcaf1a7d4006489646194018526d7bcdc09
doresahasani <dh67917@ubt-uni.net>
2024-01-23 18:19:33 +0100

    responsivitet dhe funksionalitet
    responsivitet dhe funksionalitet

--- register.js
+++ register.js
@@ -1,34 +1,31 @@
 var nameRegex=/[a-zA-z\s]+$/;
         if(!nameRegex.test(name)){
-            altert("Ju lutem shkruani nje emer valid");
+            alert("Ju lutem shkruani nje emer valid");
             return false;
         }
 
-        var UsernameRegex=/[^a-z\s]+$/;
-        if(!Username.test(Username)){
-            altert("Ju lutem shkruani nje Username valid");
+        var usernameRegex=/^[a-zA-Z0-9]+$/;
+        if(!username.test(username)){
+            alert("Ju lutem shkruani nje Username valid");
             return false;
         }
         if(password.length<7){
             alert("password duhet te jete mbi 7 karaktere");
             return false;
         }
-        var emailRegex=/^[^\s@]+@[^\s@]+\.[*\s@]+$/;
+        var emailRegex=/^[^\s@]+@[^\s@]+\.[a-zA-Z]+$/;
         if(!emailRegex.test(email)){
-            altert("Ju lutem shkruani nje Username valid");
+            alert("Ju lutem shkruani nje Username valid");
             return false;
         }
-        var countryRegex;
+        /*var countryRegex;*/
         if(ucountry.value == "Default"){
             alert("Zgjidhni vendin tuaj");
             ucountry.focus();
             return false;
         }
-        else
-        {
-            return true;
-        }
-        var GenderRegex;
+
+        var x=0;
         if(umgender.checked){
             x++;
         }if(ufgender.checked){
