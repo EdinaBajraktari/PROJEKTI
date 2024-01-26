4d6a8e1fbb055eec3f1dd979f48608f0d1ab695a
EdinaBajraktari <150193543+EdinaBajraktari@users.noreply.github.com>
2024-01-26 02:35:27 +0100

    Krijimi i news page

---
+++ news.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <link  rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta name="theme-color" content="#ffffff" />
+    <title>News </title>
+ 
+    <link rel="stylesheet" href="newsStyle.css" />
+</head>
+
+<body>
+   <header>
+
+    <div class="nav">
+        <a class="active" href="Home.php">Home</a>
+        <a href="about.php">About</a>
+        <a href="Courses.php">All Courses</a>
+        <a href="Contact.php">Contact</a>
+        <div class="search">
+       
+            <input type="text" placeholder="Search.." name="search">
+      
+        </div>
+      </div>
+
+
+<!--Intruduction i webfaqes-->
+
+<div class="text-box">
+    <h1>Latest News</h1>
+
+
+</div>
+</header>
+
+
+    <section>
+        <div class="container">
+            <h1 class="editor-h1">Editor Picks</h1>
+            <div class="articles">
+                <a href="article.html" class="card">
+                    <img src="tech1.jpg" alt="" />
+                    <article>
+                        <p class="technology-category">Technology</p>
+                        <h1>Lorem ipsum dolor sit amet.</h1>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi
+                            nisi neque eum libero maiores voluptatem repudiandae quos
+                            perspiciatis, reiciendis dolor!
+                        </p>
+                    </article>
+                </a>
+
+                <a href="article.html" class="card">
+                    <article>
+                        <p class="technology-category">Technology</p>
+                        <h1>Lorem ipsum dolor sit amet.</h1>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi
+                            nisi neque eum libero maiores voluptatem repudiandae quos
+                            perspiciatis, reiciendis dolor!
+                        </p>
+                    </article>
+                </a>
+
+                <a href="article.html" class="card">
+                    <img src="tech2.webp" alt="" />
+                    <article>
+                        <p class="technology-category">Technology</p>
+                        <h1>Lorem ipsum dolor sit amet.</h1>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi
+                            nisi neque eum libero maiores voluptatem repudiandae quos
+                            perspiciatis, reiciendis dolor!
+                        </p>
+                    </article>
+                </a>
+
+                <a href="article.html" class="card">
+                    <article>
+                        <p class="technology-category">Technology</p>
+                        <h1>Lorem ipsum dolor sit amet.</h1>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi
+                            nisi neque eum libero maiores voluptatem repudiandae quos
+                            perspiciatis, reiciendis dolor!
+                        </p>
+                    </article>
+                    <img src="tech3.avif" />
+                </a>
+
+                <a href="article.html" class="card">
+                    <img src="tech4.avif" alt="" />
+                    <article>
+                        <p class="technology-category">Technology</p>
+                        <h1>Lorem ipsum dolor sit amet.</h1>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi
+                            nisi neque eum libero maiores voluptatem repudiandae quos
+                            perspiciatis, reiciendis dolor!
+                        </p>
+                    </article>
+                </a>
+
+                <a href="article.html" class="card">
+                    <article>
+                        <p class="technology-category">Technology</p>
+                        <h1>Lorem ipsum dolor sit amet.</h1>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi
+                            nisi neque eum libero maiores voluptatem repudiandae quos
+                            perspiciatis, reiciendis dolor!
+                        </p>
+                    </article>
+                </a>
+
+                <a href="article.html" class="card">
+                    <article>
+                        <p class="technology-category">Entertainment</p>
+                        <h1>Lorem ipsum dolor sit amet.</h1>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi
+                            nisi neque eum libero maiores voluptatem repudiandae quos
+                            perspiciatis, reiciendis dolor!
+                        </p>
+                    </article>
+                    <img src="tech5.jpg" alt="" />
+                </a>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="content">
+        <div class="social-media">
+            <h4>Social</h4>
+            <p>
+              <a><i class="fab fa-twitter"></i>  Twitter</a> 
+            </p>
+            <p>
+            <a><i class="fab fa-youtube"></i>  YouTube</a>
+            </p>
+            <p>
+           <a><i class="fab fa-facebook-f"></i>  Facebook</a> 
+            </p>
+            <p>
+          <a>  <i class="fab fa-instagram"></i>   Instagram</a>
+            </p>
+          </div>
+
+          <div class="links">
+            <h4>Quick links</h4>
+
+            <p><a>Home</a></p>
+            <p><a>About</a></p>
+            <p><a>Blogs</a></p>
+            <p><a>Contact</a></p>
+
+          </div>
+
+          <div class="info">
+            <h4 class="address">Address</h4>
+            <p><a><!--<i class="fab fa-location"></i>-->
+                Kosove, Prishtine, 10000</a>
+            </p>
+            <h4 class="mobile">Mobile</h4>
+            <p><a><!--<i class="fab fa-phone"></i>-->+383 45-000-000</a></p>
+            <h4 class="mail">Email</h4>
+            <p><a><!--<i class="fab fa-envelope"></i>-->EDlearn@gmail.com</a></p>
+          </div>
+        </div>
+    </footer>
+
+    <script src="./js/script.js"></script>
+</body>
+
+</html>
