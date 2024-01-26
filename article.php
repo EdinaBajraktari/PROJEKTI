50a74917a185126b6889971b5ee81d867aacf3d0
EdinaBajraktari <150193543+EdinaBajraktari@users.noreply.github.com>
2024-01-26 02:35:46 +0100

    Krijimi i article page

---
+++ article.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <link  rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta name="theme-color" content="#ffffff" />
+    <title>News </title>
+ 
+    <link rel="stylesheet" href="newsStyle.css" />
+</head>
+
+<body>
+    <div class="nav">
+        <a class="active" href="Home.php">Home</a>
+        <a href="about.php">About</a>
+        <a href="Courses.php">All Courses</a>
+        <a href="Contact.php">Contact</a>
+        <div class="search">
+       
+            <input type="text" placeholder="Search.." name="search">
+      
+        </div>
+      </div>
+
+
+
+
+    <div class="container">
+        <section class="article-page">
+            <article>
+                <img src="tech6.jpg" alt="" />
+                <h2>Lorem ipsum dolor sit amet</h2>
+                <div>
+                    <p>
+                        Written By XYZ Janauary 14, 2024
+                    </p>
+                    <p class="technology-category">Technology</p>
+                </div>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore
+                    debitis beatae ad doloremque voluptate, blanditiis suscipit error
+                    quam fugit sapiente, aliquid quia libero incidunt recusandae enim
+                    voluptas nulla sequi similique? Temporibus ad est minima magni
+                    molestias obcaecati consequatur cumque exercitationem deleniti eos,
+                    animi facere recusandae, pariatur eveniet neque? Voluptatem,
+                    molestias.
+                </p>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae
+                    dignissimos dolorum ipsam corporis similique! Quis, esse minus?
+                    Ducimus tempore provident sunt officia? Velit temporibus harum
+                    voluptate, ipsum quisquam rerum doloremque consectetur quos incidunt
+                    deleniti dicta excepturi ratione, fugiat ad fuga in nam dolorum aut
+                    minus esse voluptatum suscipit et quae. Repellendus minus, ratione
+                    aperiam commodi voluptas iusto aliquam sapiente. Aut obcaecati,
+                    corporis praesentium porro nostrum excepturi animi repellendus
+                    incidunt non quis nisi assumenda voluptas possimus molestiae
+                    laboriosam iure sed architecto in perferendis magnam blanditiis
+                    voluptatum placeat labore odio? Unde id dolore aliquam, quas sit
+                    harum, saepe maiores facilis aliquid molestiae qui. Praesentium
+                    rerum, minima fugit laudantium quaerat voluptatem pariatur sequi
+                    saepe blanditiis nobis natus possimus quo, assumenda numquam sit,
+                    eaque accusamus incidunt exercitationem quis deleniti quisquam
+                    corrupti vel? Ut quisquam consequatur, doloribus voluptatibus iusto
+                    quaerat, sint non magnam reiciendis amet quos sapiente excepturi
+                    omnis laboriosam nobis, expedita delectus odio provident?
+                </p>
+            </article>
+
+            <article>
+                <h3>CATEGORIES</h3>
+                <ul>
+                    <li>Sports</li>
+                    <li>Entertainment</li>
+                    <li>Technology</li>
+                    <li>Fashion</li>
+                   
+                </ul>
+            </article>
+
+      
+        </section>
+    </div>
+
+    <footer>
+        <div class="content">
+        <div class="social-media">
+            <h4>Social</h4>
+            <p>
+              <a><i class="fab fa-twitter"></i>  Twitter</a> 
+            </p>
+            <p>
+            <a><i class="fab fa-youtube"></i>  YouTube</a>
+            </p>
+            <p>
+           <a><i class="fab fa-facebook-f"></i>  Facebook</a> 
+            </p>
+            <p>
+          <a>  <i class="fab fa-instagram"></i>   Instagram</a>
+            </p>
+          </div>
+
+          <div class="links">
+            <h4>Quick links</h4>
+
+            <p><a>Home</a></p>
+            <p><a>About</a></p>
+            <p><a>Blogs</a></p>
+            <p><a>Contact</a></p>
+
+          </div>
+
+          <div class="info">
+            <h4 class="address">Address</h4>
+            <p><a><!--<i class="fab fa-location"></i>-->
+                Kosove, Prishtine, 10000</a>
+            </p>
+            <h4 class="mobile">Mobile</h4>
+            <p><a><!--<i class="fab fa-phone"></i>-->+383 45-000-000</a></p>
+            <h4 class="mail">Email</h4>
+            <p><a><!--<i class="fab fa-envelope"></i>-->EDlearn@gmail.com</a></p>
+          </div>
+        </div>
+    </footer>
+
+    <script src="./js/script.js"></script>
+</body>
+
+</html>
